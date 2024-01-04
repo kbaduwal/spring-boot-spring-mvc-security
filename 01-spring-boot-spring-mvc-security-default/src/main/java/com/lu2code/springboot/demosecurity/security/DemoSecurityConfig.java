d167a150f351ba615581abdeea166d2683e3e114
kbaduwal <khakendra1999@gmail.com>
2024-01-04 22:11:38 +0530

    1.Adding access denied page, 2. Display content based on user Roles 3. Adding JDBC Authentication

--- 01-spring-boot-spring-mvc-security-default/src/main/java/com/lu2code/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 01-spring-boot-spring-mvc-security-default/src/main/java/com/lu2code/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -3,14 +3,46 @@ package com.lu2code.springboot.demosecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
+    // add support for JDBC ... no more hard coded users
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
 
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers("/").hasRole("EMPLOYEE")
+                        .requestMatchers("/leaders/**").hasRole("MANAGER")
+                        .requestMatchers("/systems/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form ->
+                        form
+                                .loginPage("/showMyLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+                )
+                .logout(logout ->logout.permitAll()
+                )
+                .exceptionHandling(configurer ->
+                        configurer.accessDeniedPage("/access-denied"));
+
+        return http.build();
+    }
+
+
+    /*
     @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
         UserDetails john = User.builder()
@@ -35,26 +67,8 @@ public class DemoSecurityConfig {
         return new InMemoryUserDetailsManager(john,mary,susan);
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
-        http.authorizeHttpRequests(configurer ->
-                configurer
-                        .requestMatchers("/").hasRole("EMPLOYEE")
-                        .requestMatchers("/leaders/**").hasRole("MANAGER")
-                        .requestMatchers("/systems/**").hasRole("ADMIN")
-                        .anyRequest().authenticated()
-                )
-                .formLogin(form ->
-                        form
-                                .loginPage("/showMyLoginPage")
-                                .loginProcessingUrl("/authenticateTheUser")
-                                .permitAll()
-                )
-                .logout(logout ->logout.permitAll()
-                );
+ */
 
-        return http.build();
-    }
 }
 
 
