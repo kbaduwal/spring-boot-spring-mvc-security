733b9f2c0da2dd512313d5d88ed22128e75a46f9
kbaduwal <khakendra1999@gmail.com>
2024-01-05 21:04:03 +0530

    1. Adding Sprig Security Password Encryption 2. Configuration of Spring security to use our custom database tables

--- 01-spring-boot-spring-mvc-security-default/src/main/java/com/lu2code/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 01-spring-boot-spring-mvc-security-default/src/main/java/com/lu2code/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -14,8 +14,15 @@ public class DemoSecurityConfig {
     // add support for JDBC ... no more hard coded users
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource){
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?");
 
-        return new JdbcUserDetailsManager(dataSource);
+        // define query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?");
+        return jdbcUserDetailsManager;
     }
 
     @Bean
